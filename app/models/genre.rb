723ae09ec1edc01c77a14cc70a7ecd4b922fc5f2
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-21 03:29:27 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,8 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.collect do |artist| 
-      artist.name 
-    end 
+    self.artists.collect { |artist| artist.name }
   end
-end
+  
+end #end of the class 
